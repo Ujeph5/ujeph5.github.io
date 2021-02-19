33275647adfa28883ed08abfdcfbd23dfe166625
Ujeph5 <j.s.alexander.v@gmail.com>
2021-02-18 23:14:51 -0300

    added alarm.html

---
+++ js/alarm.js
@@ -0,0 +1,18 @@
+//input - no user input, the program will get the date from the computer
+//processing - determine whether a day is a weekday, weekend or holiday
+//output - display "sleep in" or "get up" depending on the day
+function alarm() {
+    let now = new Date();
+    let month = now.getMonth();
+    let dayOfMonth = now.getDate();
+    let dayOfWeek = now.getDay();
+    let message;
+    if (month == 0 && dayOfMonth == 1 || month == 6 && dayOfMonth == 4 || month == 11 && dayOfMonth == 25) {
+        message = "Sleep in.";
+    } else if (dayOfWeek >= 1 && dayOfWeek <=5) {
+        message = "Get up!";
+    } else {
+        message = "Sleep in.";
+    }
+    document.getElementById('output').innerHTML = message;
+}
